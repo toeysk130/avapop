a9b94610b2e967f21dc75fdc4c2846ccaad01a58
Oscar <56079903+OscarNOW@users.noreply.github.com>
2024-07-26 13:51:07 +0200

    Created simple hello world webserver

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import express from 'express';
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    return res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
